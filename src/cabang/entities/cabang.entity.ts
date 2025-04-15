a317191cfc7ddff9bda4030ec14408382c3a89f2
afesokta <afes.okta@gmail.com>
2025-04-16 01:32:30 +0700

    feat:add-entity-master-cabang

--- src/cabang/entities/cabang.entity.ts
+++ src/cabang/entities/cabang.entity.ts
@@ -1,1 +1,36 @@
-export class Cabang {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Timestamp } from 'typeorm';
+
+const idType = Number;
+
+export class Cabang {
+  @ApiProperty({
+    type: idType,
+  })
+  id: number | string;
+  @ApiProperty({
+    type: String,
+    example: 'cabang1',
+  })
+  cabang: string;
+  @ApiProperty({
+    type: Number,
+    example: '1',
+  })
+  createdBy: number;
+  @ApiProperty({
+    type: Timestamp,
+    example: new Date(),
+  })
+  createdAt: Timestamp;
+  @ApiProperty({
+    type: Number,
+    example: '1',
+  })
+  updatedBy: number;
+  @ApiProperty({
+    type: Timestamp,
+    example: new Date(),
+  })
+  updatedAt: Timestamp;
+}
